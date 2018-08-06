5149416dd9baa52fab71f278b916fb830034e47e
charankrishna <charan.nithinj@gmail.com>
2018-08-06 11:40:40 +0530

    MultipleModules Export

---
+++ multipleModules/aapp.js
@@ -0,0 +1,3 @@
+var MyMethods = require('./index.js');
+var method = MyMethods.method;
+var otherMethod = MyMethods.otherMethod;
---
+++ multipleModules/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    method: function() {},
+    otherMethod: function() {}
+}
+
+//
+//exports.method = function() {};
+//exports.otherMethod = function() {};
+
